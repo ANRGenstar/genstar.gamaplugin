889bf3fcd3db33e6e2325a7c74bc0e55b0cd019b
Huynh Quang-Nghi <hqnghi88@gmail.com>
2018-12-12 01:17:33 +0700

    permission

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd genstar.gamaplugin.p2updatesite &&
-mvn -U clean install -P p2Repo --settings ../settings.xml && 
+mvn -U -X clean install -P p2Repo --settings ../settings.xml && 
 cd -
