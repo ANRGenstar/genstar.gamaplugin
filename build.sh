130e662a8a17ee6f113996da66d9d2b1db02103e
benoitgaudou <benoit.gaudou@gmail.com>
2019-08-14 09:16:50 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 cd genstar.gamaplugin.parent &&
 mvn clean install -U &&
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=ANRGenstar_genstar.gamaplugin &&
 cd -
